0a32c6230eebcba4f1d0f7914a56ff36ddffccfb
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-04-08 23:54:08 -0300

    Create the style and html elements, and create function submite forms

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,124 +1,95 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import logo from "./../../public/toDoLogo.svg";
+import { Inter } from "next/font/google";
+import { Clipboard } from "phosphor-react";
+import { useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
+
+interface taskProps {
+  id: number;
+  isComplete: boolean;
+  task: string;
+}
 
 export default function Home() {
+  const [task, setTask] = useState("");
+  const [tasksArray, setTasksArray] = useState<taskProps[]>([]);
+
+  function handleTask(event: any) {
+    setTask(event.target.value);
+  }
+
+  function handleSubmitTask(event: any, data: taskProps) {
+    event?.preventDefault();
+    const newTask = {
+      id: tasksArray.length + 1,
+      isComplete: false,
+      task: task,
+    };
+
+    setTasksArray([...tasksArray, newTask]);
+  }
+
+  console.log(tasksArray);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
+    <>
+      <header className="bg-gray-900 flex justify-center items-center h-52 ">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src={logo}
+          alt="Logo de um foguete com o nome ToDo escrito"
+          className="w-32 h-12"
         />
-      </div>
+      </header>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <main className="bg-gray-500 flex justify-center h-screen relative">
+        <div
+          className="max-w-2xl absolute top-[-27px] 
+        "
         >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <form className="w-full flex gap-2" onSubmit={() => handleSubmitTask}>
+            <input
+              type="text"
+              onChange={handleTask}
+              className="h-[54px] min-w-[638px] bg-[#262626] border border-solid border-gray-900 rounded-lg"
+            />
+            <button
+              type="submit"
+              className="h-full w-[90px] flex items-center justify-center"
+            >
+              Criar
+            </button>
+          </form>
+          <div className="mt-4">
+            <div className="flex justify-between">
+              <div className="flex gap-2">
+                Tarefas criadas{" "}
+                <span className="w-[25px] flex items-center justify-center pt-[2px] pr-[8px] pb-[2px] pl-[8px] bg-[#333333] rounded-[999px] text-white">
+                  0
+                </span>
+              </div>
+              <div className="flex gap-2">
+                Concluídas{" "}
+                <span className="w-[25px] flex items-center justify-center pt-[2px] pr-[8px] pb-[2px] pl-[8px] bg-[#333333] rounded-[999px] text-white">
+                  0
+                </span>
+              </div>
+            </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+            <div className="min-h-[245px] border-t border-solid border-gray-600 mt-4 flex items-center justify-center">
+              <div className="flex items-center justify-center flex-col">
+                <Clipboard className="w-14 h-14" />
+                <strong className="font-inter font-bold text-lg leading-relaxed text-center">
+                  Você ainda não tem tarefas cadastradas <br />
+                </strong>
+                <span className="font-normal text-center w-full block">
+                  Crie tarefas e organize seus itens a fazer
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
